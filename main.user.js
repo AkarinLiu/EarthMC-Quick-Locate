9ccd5a919a75728e3c490371de8521bd0b989734
阿卡林刘 <akarinliu@outlook.com>
2025-02-15 15:29:15 +0800

    Update main.user.js
    更改 自动更新 URL

--- main.user.js
+++ main.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         EarthMC 地图快速定位
-// @version      2025.2
-// @updateURL    https://cdn.jsdelivr.net/gh/AkarinLiu/EarthMC-Quick-Locate@master-cdn/main.user.js
-// @downloadURL  https://cdn.jsdelivr.net/gh/AkarinLiu/EarthMC-Quick-Locate@master-cdn/main.user.js
+// @version      2025.3
+// @updateURL    https://cdn.jsdelivr.net/gh/AkarinLiu/EarthMC-Quick-Locate@master/main.user.js
+// @downloadURL  https://cdn.jsdelivr.net/gh/AkarinLiu/EarthMC-Quick-Locate@master/main.user.js
 // @description  始终可用的坐标定位面板，支持参数覆盖和即时更新
 // @author       AkarinLiu
 // @match        https://map.earthmc.net/*
